c59444e6f4eb620941b9d694cceeee5d5f0ea54a
Màrius Comas Rosa <mariuscr23@gmail.com>
2026-09-22 12:12:32 +0200

    Add apple icon and SVG icon for application branding
    - Added a new PNG file for the apple icon located at src/app/apple-icon.png.
    - Introduced a new SVG file for the application icon located at src/app/icon.svg, with styles for light and dark themes.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,11 +15,10 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#efebe7",
     theme_color: "#0b0b0b",
     icons: [
-      {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
+      { src: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+      { src: "/icon.svg", sizes: "any", type: "image/svg+xml" },
+      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
   };
 }
